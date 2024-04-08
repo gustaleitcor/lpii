7bce9c51bb1d2903d457ecc71c189dca00a2a42f
gustaleitcor <gustavo.leitecordeiro@gmail.com>
2024-04-08 12:28:44 -0300

    comentando factory.c

--- factory.c
+++ factory.c
@@ -8,85 +8,101 @@
 #define N_MACHINE 30
 #define N_FACTORY 15
 
-#define MAX_TIME_FACTORY 5
-#define MAX_TIME_MACHINE 10
-#define MAX_TIME_TRUCK 10
+#define MAX_TIME_FACTORY 5  // tempo maximo de produção
+#define MAX_TIME_MACHINE 10 // tempo maximo de consumo das maquinas
+#define MAX_TIME_TRUCK 10   // tempo maximo de consumo dos caminhões
 
-#define WAIT_TIME 5
-#define SLOWDOWN 250000
+#define WAIT_TIME 5     // tempo de espera caso o deposito estiver vazio
+#define SLOWDOWN 250000 // desaceleração prosital de execução
 
 typedef struct Deposit_t {
   size_t machines_itens, truck_itens;
 } Deposit;
 
-Deposit deposit = {0};
-pthread_mutex_t deposit_access;
-pthread_barrier_t start_barrier;
+Deposit deposit = {0}; // inicialização do deposito
+
+pthread_mutex_t deposit_access; // mutex de acesso à variavel de acesso
+                                // compartilhada (deposito)
+
+pthread_barrier_t start_barrier; // barreira de inicialização
 
 void *factory(void *p) {
-  pthread_barrier_wait(&start_barrier);
+  pthread_barrier_wait(
+      &start_barrier); // espera até que todos as estruturas estejam prontas
 
   long id = (long)p;
 
   while (1) {
-    pthread_mutex_lock(&deposit_access);
+    sleep(rand() % MAX_TIME_FACTORY + 1); // simula tempo de produção
+    pthread_mutex_lock(
+        &deposit_access); // garante o acesso à variavel compartilhada
+
     if (rand() % 2) {
-      deposit.machines_itens++;
+      deposit.machines_itens++; // produz item de maquina
       printf("[FACTORY %ld] produziu item de maquina\n", id);
     } else {
-      deposit.truck_itens++;
+      deposit.truck_itens++; // produz item de caminhão
       printf("[FACTORY %ld] produziu item de caminhao\n", id);
     }
-    usleep(SLOWDOWN);
-    pthread_mutex_unlock(&deposit_access);
-    sleep(rand() % MAX_TIME_FACTORY + 1);
+
+    usleep(SLOWDOWN); // desacelera propositalmente o programa
+    pthread_mutex_unlock(
+        &deposit_access); // libera o acesso à variavel compartilhada
   }
 }
 
 void *machine(void *p) {
-  pthread_barrier_wait(&start_barrier);
+  pthread_barrier_wait(
+      &start_barrier); // espera até que todos as estruturas estejam prontas
 
   long id = (long)p;
 
   while (1) {
-    pthread_mutex_lock(&deposit_access);
+    pthread_mutex_lock(
+        &deposit_access); // garante o acesso à variavel compartilhada
 
-    if (deposit.machines_itens != 0) {
+    if (deposit.machines_itens != 0) { // verifica se o existem itens
 
-      deposit.machines_itens--;
+      deposit.machines_itens--; // se existir, consome item
       printf("[MACHINE %ld] consumiu item\n", id);
-      usleep(SLOWDOWN);
+      usleep(SLOWDOWN); // desacelera propositalmente o programa
 
-      pthread_mutex_unlock(&deposit_access);
-      sleep(rand() % MAX_TIME_MACHINE + 1);
+      pthread_mutex_unlock(
+          &deposit_access); // libera o acesso à variavel compartilhada
+      sleep(rand() % MAX_TIME_MACHINE + 1); // simula tempo de consumo
     } else {
-      pthread_mutex_unlock(&deposit_access);
-      sleep(WAIT_TIME);
+      pthread_mutex_unlock(&deposit_access); // se não existir, libera acesso à
+                                             // variavel compartilhada
+      sleep(WAIT_TIME); // espera até proxima checagem
     }
   }
 }
 
 void *truck(void *p) {
-  pthread_barrier_wait(&start_barrier);
+  pthread_barrier_wait(
+      &start_barrier); // espera até que todos as estruturas estejam prontas
 
   long id = (long)p;
 
   while (1) {
-    pthread_mutex_lock(&deposit_access);
+    pthread_mutex_lock(
+        &deposit_access); // garante o acesso à variavel compartilhada
 
-    if (deposit.truck_itens != 0) {
+    if (deposit.truck_itens != 0) { // verifica se o existem itens
 
-      deposit.truck_itens--;
+      deposit.truck_itens--; // se existir, consome item
       printf("[TRUCK %ld] consumiu item\n", id);
 
-      usleep(SLOWDOWN);
+      usleep(SLOWDOWN); // desacelera propositalmente o programa
 
-      pthread_mutex_unlock(&deposit_access);
+      pthread_mutex_unlock(
+          &deposit_access); // libera o acesso à variavel compartilhada
 
-      sleep(rand() % MAX_TIME_TRUCK + 1);
+      sleep(rand() % MAX_TIME_TRUCK + 1); // simula tempo de consumo
     } else {
-      pthread_mutex_unlock(&deposit_access);
-      sleep(WAIT_TIME);
+      pthread_mutex_unlock(&deposit_access); // se não existir, libera acesso à
+                                             // variavel compartilhada
+      sleep(WAIT_TIME); // espera até proxima checagem
     }
   }
 }
@@ -100,22 +116,25 @@ int main() {
   pthread_t factory_thread[N_FACTORY], machine_threads[N_MACHINE],
       truck_threads[N_TRUCKS];
 
-  pthread_mutex_init(&deposit_access, NULL);
+  pthread_mutex_init(&deposit_access,
+                     NULL); // inicializa mutex do acesso ao deposito
   pthread_barrier_init(&start_barrier, NULL,
-                       N_FACTORY + N_MACHINE + N_TRUCKS + 1);
+                       N_FACTORY + N_MACHINE + N_TRUCKS +
+                           1); // inicializa barreira de inicialização
 
-  for (long i = 0; i < N_FACTORY; i++)
+  for (long i = 0; i < N_FACTORY; i++) // cria as threads das fabricas
     pthread_create(&factory_thread[i], NULL, factory, (void *)i);
 
-  for (long i = 0; i < N_MACHINE; i++)
+  for (long i = 0; i < N_MACHINE; i++) // cria as threads das maquinas
     pthread_create(&machine_threads[i], NULL, machine, (void *)i);
 
-  for (long i = 0; i < N_TRUCKS; i++)
+  for (long i = 0; i < N_TRUCKS; i++) // cria as threads dos caminhões
     pthread_create(&machine_threads[i], NULL, truck, (void *)i);
 
-  pthread_barrier_wait(&start_barrier);
+  pthread_barrier_wait(
+      &start_barrier); // espera até que todos as estruturas estejam prontas
 
-  while (1) {
+  while (1) { // printa o estado atual do deposito
     sleep(2);
     printf("[DEPOSIT] |machine_itens: %zu | truck_itens: %zu|\n",
            deposit.machines_itens, deposit.truck_itens);
